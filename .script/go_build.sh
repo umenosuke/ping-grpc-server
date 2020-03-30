4997f82c4d1fe4ce32c5cddecc16f2b21f95ecd7
umenosuke <umenosuke.net@gmail.com>
2020-03-30 12:33:09 +0900

    ビルドスクリプトの修正

--- .script/go_build.sh
+++ .script/go_build.sh
@@ -10,7 +10,7 @@ _ARCH=${2}
 _SRC_PATH=${3}
 _OUTPUT_PATH=${4}
 
-GOOS="${_OS}" GOARCH="${_ARCH}" go build \
+CGO_ENABLED=0 GOOS="${_OS}" GOARCH="${_ARCH}" go build \
  -ldflags "-X main.metaVersion=${_GIT_TAG} -X main.metaRevision=${_GIT_HASH}" \
  -o "${_OUTPUT_PATH}" \
  "${_SRC_PATH}"
